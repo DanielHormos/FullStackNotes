4257ee1530c851561e393e29d2179ebaa9c433f2
Daniel Hormos <daniel_danne32@hotmail.com>
2024-10-13 22:45:31 +0200

    changed test so input when creating a note is a object and not a string and test works :white_check_mark:

--- backend/src/lib.test.ts
+++ backend/src/lib.test.ts
@@ -3,13 +3,13 @@ import { addNote, getNotes } from "./lib";
 
 describe(`Note functions`, () => {
   it("Should add a new note and return it", () => {
-    const note = addNote("note1");
+    const note = addNote({ content: "note1" });
 
     expect(note.content).toBe("note1");
   });
 
   it("should get all notes", () => {
-    addNote("note2");
+    addNote({ content: "note2" });
 
     const result = getNotes();
     expect(result[0].content).toBe("note1");
