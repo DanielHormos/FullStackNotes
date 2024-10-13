26f2e5669f8e90ed5afceee4f6283cc995cfb1f1
Daniel Hormos <daniel_danne32@hotmail.com>
2024-10-13 14:56:28 +0200

    added local array to store notes and added function to add Note :sparkles:

--- backend/src/lib.ts
+++ backend/src/lib.ts
@@ -1,3 +1,16 @@
 export function getResponseText(): string {
   return "Response message fron lib.ts file";
 }
+
+type Note = {
+  id: number;
+  content: string;
+};
+
+const notes: Note[] = [];
+
+export function addNote(content: string): Note {
+  const newNote = { id: notes.length + 1, content };
+  notes.push(newNote);
+  return newNote;
+}
